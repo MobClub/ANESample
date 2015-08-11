9838c020416b7bc038505b06a67d4f59182056bf
zhangBrook <1449282202@qq.com>
2015-08-11 14:42:28 +0800

    Update ShareSDK To V2.6.2

---
+++ Android/ShareSDKForANE/src/cn/sharesdk/ane/apshare/ShareEntryActivity.java
@@ -0,0 +1,7 @@
+package cn.sharesdk.ane.apshare;
+
+import cn.sharesdk.alipay.share.AlipayHandlerActivity;
+
+public class ShareEntryActivity extends AlipayHandlerActivity{
+   
+}
--- Android/ShareSDKForANE/src/cn/sharesdk/onekeyshare/OnekeyShare.java
+++ Android/ShareSDKForANE/src/cn/sharesdk/onekeyshare/OnekeyShare.java
@@ -314,15 +314,25 @@ public class OnekeyShare implements PlatformActionListener, Callback {
 			plat.SSOSetting(disableSSO);
 			String name = plat.getName();
 
-//			boolean isGooglePlus = "GooglePlus".equals(name);
-//			if (isGooglePlus && !plat.isValid()) {
-//				Message msg = new Message();
-//				msg.what = MSG_TOAST;
-//				int resId = getStringRes(context, "google_plus_client_inavailable");
-//				msg.obj = context.getString(resId);
-//				UIHandler.sendMessage(msg, this);
-//				continue;
-//			}
+			boolean isGooglePlus = "GooglePlus".equals(name);
+			if (isGooglePlus && !plat.isClientValid()) {
+				Message msg = new Message();
+				msg.what = MSG_TOAST;
+				int resId = getStringRes(context, "google_plus_client_inavailable");
+				msg.obj = context.getString(resId);
+				UIHandler.sendMessage(msg, this);
+				continue;
+			}
+
+			boolean isAlipay = "Alipay".equals(name);
+			if (isAlipay && !plat.isClientValid()) {
+				Message msg = new Message();
+				msg.what = MSG_TOAST;
+				int resId = getStringRes(context, "alipay_client_inavailable");
+				msg.obj = context.getString(resId);
+				UIHandler.sendMessage(msg, this);
+				continue;
+			}
 
 			boolean isKakaoTalk = "KakaoTalk".equals(name);
 			if (isKakaoTalk && !plat.isClientValid()) {
